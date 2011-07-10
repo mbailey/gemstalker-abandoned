9265f141b99ea04cb616639473110b7879101a2d
Mike Bailey <mike@bailey.net.au>
2011-07-10 23:12:55 +1000

    Setup a standard password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ActiveRecord::Base
   has_many :follows
   has_many :rubygems, :through => :follows
 
-  # validates_presence_of :password, :on => :create
+  validates_presence_of :password, :on => :create
   validates_uniqueness_of :email
+
+  # We're not asking new users to supply a password
+  before_validation(:on => :create) do 
+    self.password = self.password_confirmation = 'foobar'
+  end
 
 end
