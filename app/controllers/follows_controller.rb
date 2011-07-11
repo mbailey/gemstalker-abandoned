e7a341e493714c8931d5271eafe7c099f6f81f47
Mike Bailey <mike@bailey.net.au>
2011-07-12 01:23:21 +1000

    Moved the flash out into the body

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,7 +2,10 @@ class FollowsController < ApplicationController
 
   def create
     @rubygem = Rubygem.find_or_create_by_name(params[:rubygem])
-    if current_user.rubygems.find_by_name(params[:rubygem]) 
+    if current_user.errors
+      redirect_to rubygem_path(@rubygem.name), 
+        :notice => "That's not a valid email address"
+    elsif current_user.rubygems.find_by_name(params[:rubygem]) 
       redirect_to rubygem_path(@rubygem.name), 
         :notice => "You were already following #{@rubygem.name}"
     elsif current_user.rubygems << @rubygem 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    record.errors[attribute] << (options[:message] || "is not an email") unless
+    value =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  end
+end
+
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   has_secure_password
@@ -5,7 +12,7 @@ class User < ActiveRecord::Base
   has_many :rubygems, :through => :follows
 
   validates_presence_of :password, :on => :create
-  validates_uniqueness_of :email
+  validates :email, :presence => true, :email => true
 
   # We're not asking new users to supply a password
   before_validation(:on => :create) do 
