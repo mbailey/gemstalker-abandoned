f5335bfd4497ff2f4fb05c057075d77ed9b6606d
Mike Bailey <mike@bailey.net.au>
2011-07-11 11:57:41 +1000

    not needed

--- app/models/release_observer.rb
+++
@@ -1,8 +0,0 @@
-class ReleaseObserver < ActiveRecord::Observer
-  def after_create(release)
-    release.follows.each do |follow|
-      logger.info "Email #{follow.user_email} about release of 
-        #{release.rubygem_name}-#{release.version}"
-    # code to send notifications
-  end
-end
