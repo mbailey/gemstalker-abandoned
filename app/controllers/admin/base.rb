0effaa7aa1f0f5393552f1e54b24655f0a343710
Mike Bailey <mike@bailey.net.au>
2011-07-11 12:13:59 +1000

    bugfix

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,7 +1,7 @@
 class Admin::Base < ApplicationController
   protect_from_forgery
   # before_filter :require_admin
-  session :disabled => false
+  # session :disabled => false
   
   # layout 'admin'
 end
