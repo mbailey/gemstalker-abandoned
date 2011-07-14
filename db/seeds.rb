f3c322f4e92eb2f0d1c652d2b10e26cd83465fce
Mike Bailey <mike@bailey.net.au>
2011-07-14 22:42:45 +1000

    Added a couple of follows to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Rubygem.create([{ name: 'gemstalk' }, { name: 'rails' }])
+Rubygem.create([{ name: 'gemstalk' }, 
+                { name: 'deprec' },
+                { name: 'rails' }])
 
 user = User.create(:email => 'mike@bailey.net.au')
 
 user.rubygems << Rubygem.find_by_name('gemstalk')
+user.rubygems << Rubygem.find_by_name('deprec')
